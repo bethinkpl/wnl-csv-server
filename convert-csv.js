4c494353c931629c4f35f377f61db1b15c215d45
Jakub Wojtczak <jjwojtczak@gmail.com>
2021-09-20 10:50:56 +0200

    Server converted to netlify

---
+++ convert-csv.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const csv = require("csv-parser");
+
+const readData = (dataFile, cb) => {
+  let results = [];
+  fs.createReadStream(dataFile)
+    .on("error", (err) => console.log("Error:", err.message))
+    .pipe(csv())
+    .on("data", (csvData) => results.push(csvData))
+    .on("end", () => {
+      cb(results);
+    });
+};
+
+readData("./data.csv", (results) => {
+  const content = `exports.results = ${JSON.stringify(results)};`;
+  fs.writeFile("./netlify/functions/results.js", content, 'utf-8', (err) => {
+    if (err) {
+      console.error("Could not write to json:", err);
+    } else {
+      console.log("Success");
+    }
+  });
+});
--- netlify/functions/data.js
+++ netlify/functions/data.js
@@ -1,6 +1,18 @@
-exports.handler = async function() {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({}),
-  };
+const results = require("./results");
+
+exports.handler = async function(event) {
+
+  const items = results.filter((item) => item.user_id === event.queryStringParameters["uid"]);
+
+  if (items.length > 0) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(item[0]),
+    };
+  } else {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "Data not found" }),
+    };
+  }
 }
